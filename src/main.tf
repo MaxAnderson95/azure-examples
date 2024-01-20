00b12368920600d1cb670d7feb24e746d42b8056
Max Anderson <max.a.anderson95@gmail.com>
2024-01-19 19:52:13 -0500

    Initial commit

---
+++ src/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "maxandersontfstate"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.87.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  global_tags = {
+    managed_by = "terraform"
+  }
+}
