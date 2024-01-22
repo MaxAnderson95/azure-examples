f53833f1d6fa308fc9f49c1662b167666852498a
Max Anderson <max.a.anderson95@gmail.com>
2024-01-21 21:29:53 -0500

    removed route table stuff

--- 01-single-vnet-natgw/src/vnet.tf
+++ 01-single-vnet-natgw/src/vnet.tf
@@ -11,17 +11,3 @@ resource "azurerm_subnet" "workload1" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [cidrsubnet(var.address_space[0], 8, 1)]
 }
-
-resource "azurerm_route_table" "workload1" {
-  name                = "workload1"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-}
-
-resource "azurerm_route" "default" {
-  name                = "default"
-  resource_group_name = azurerm_resource_group.rg.name
-  route_table_name    = azurerm_route_table.workload1.name
-  address_prefix      = "0.0.0.0/0"
-  next_hop_type = 
-}
