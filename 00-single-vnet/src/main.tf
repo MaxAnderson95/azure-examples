f716549e02fc02058d6dff6e985d11c0eeef1f95
Max Anderson <max.a.anderson95@gmail.com>
2024-01-23 14:50:44 -0500

    Added a basic 00 example

---
+++ 00-single-vnet/src/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "maxandersontfstate"
+    container_name       = "tfstate"
+    key                  = "00-single-vnet.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.87.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  numeral_prefix = "00"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.numeral_prefix}-single-vnet"
+  location = var.region
+}
